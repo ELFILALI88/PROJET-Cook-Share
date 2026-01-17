a3375b9dd01baa7fc5623089e58026df54af9896
ELFILALI88 <filalielfi18@gmail.com>
2026-01-17 22:34:24 +0100

    Membre 3 : modification, suppression et sécurité des recettes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
 from . import views
-
+from .views import recipe_edit, recipe_delete
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
     path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
-]
+    path('recipe/<int:id>/edit/', recipe_edit, name='recipe_edit'),
+path('recipe/<int:id>/delete/', recipe_delete, name='recipe_delete'),
 
+]
 
 
